dc3c324e3f84ed0b0ffe0e5c4949ba5b9bbd3415
miles waugh <miles.m.waugh@gmail.com>
2022-12-04 14:50:29 -0800

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,8 +1,8 @@
 from comptester import tester
 
 # Defining parameters
-test1 = 'x*x*x'  # Multiplication
-test2 = 'x**3'  # Power
+test1 = 'x*x*x*x*x*x'  # Multiplication
+test2 = 'x**6'  # Power
 iterations = 100000
 samples = 100
 
