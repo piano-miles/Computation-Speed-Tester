939d77f81dd263ca37c87f49bccd22dc18b50832
miles waugh <miles.m.waugh@gmail.com>
2022-12-04 14:52:30 -0800

    Update

--- example/example.py
+++ example/example.py
@@ -1,12 +1,12 @@
 from comptester import tester
 
 # Defining parameters
-test1 = 'x*x*x*x*x*x'  # Multiplication
-test2 = 'x**6'  # Power
-iterations = 100000
-samples = 100
+test1 = 'x*x*x*x*x*x'  # Compute x^6 by multiplication
+test2 = 'x**6'  # Compute x^6 by power
+iterations = 100000  # Number of times each expression is tested in a sample
+samples = 100  # Number of samples to collect
 
-# Run the test
+# Run the test and store it in the 'test' variable
 test = tester.test(test1, test2, iterations, samples)
 
 # Print the test results to the console
